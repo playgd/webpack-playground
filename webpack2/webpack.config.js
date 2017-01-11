cb1de5b87d90564d8e51c26f36294bcd3e67287a
Fernando Daciuk <f.daciuk@gmail.com>
2017-01-10 22:11:25 -0200

    Try use glob on webpack entry

--- webpack2/webpack.config.js
+++ webpack2/webpack.config.js
@@ -1,9 +1,31 @@
 'use strict'
 
+const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin')
+const glob = require('glob')
+
+const allFiles = glob.sync('./src/**/*.js')
+
+const getChunkName = (fileName) => {
+  return fileName.replace('./src/', '').replace(/\.js$/, '')
+}
+
+const getFilesList = (acc, f) => Object.assign({}, acc, {
+  [getChunkName(f)]: f
+})
+
 module.exports = {
-  entry: './src/index.js',
+  entry: allFiles.reduce(getFilesList, {}),
+
   output: {
     filename: 'dist/[name].js',
+    library: '[name]',
     libraryTarget: 'amd'
-  }
+  },
+
+  plugins: allFiles.map((f) => (
+    new CommonsChunkPlugin({
+      name: getChunkName(f),
+      chunks: [f]
+    })
+  ))
 }
