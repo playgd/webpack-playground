dd2a6e2380f0a11a196cafd19e9e1b6b29c05846
Fernando Daciuk <f.daciuk@gmail.com>
2017-04-22 12:32:09 -0300

    HRATT - Load React component on server side

--- hot-reload-all-the-things/server/server.js
+++ hot-reload-all-the-things/server/server.js
@@ -1,9 +1,30 @@
 import express from 'express'
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import App from '../common/app'
 
 const app = express()
 
 app.get('/api', (req, res) => {
   res.send({ message: 'I am a server route and can also be hot reloaded!' })
+})
+
+app.get('*', (req, res) => {
+  const application = renderToString(<App />)
+  const html = `<!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8" />
+        <title>Hot reload all the things!</title>
+      </head>
+
+      <body>
+        <div data-js="root">${application}</div>
+      </body>
+    </html>
+  `
+
+  res.send(html)
 })
 
 export default app
