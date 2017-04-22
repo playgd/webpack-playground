9683c4ab6f009923291dde807d50d1d13372d167
Fernando Daciuk <f.daciuk@gmail.com>
2017-04-22 12:00:08 -0300

    Remove hot-reload-all-the-things to start again

--- hot-reload-all-the-things/src/server/content.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-export default {
-  message: 'I am a server route and can also be hot reloaded!'
-}
--- hot-reload-client-server/common/title.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-import React from 'react'
-
-const Title = ({ children }) => (
-  <h1>{children}</h1>
-)
-
-export default Title
