ee3a60acab9a6f68157e319d735ab1e8f3125ccf
Fernando Daciuk <f.daciuk@gmail.com>
2017-05-11 22:51:37 -0300

    Update plugin to class

--- static-site/plugins/spa-webpack-plugin.js
+++ static-site/plugins/spa-webpack-plugin.js
@@ -1,19 +1,19 @@
 'use strict'
 
-function SPAWebpackPlugin (options) {}
-
-const beforeHtml = (htmlPluginData, callback) => {
-  const serverCode = require('../build/js/server')
-  htmlPluginData.html = htmlPluginData.html.replace('<!-- server -->', serverCode)
-  callback(null, htmlPluginData)
+class SPAWebpackPlugin {
+  apply (compiler) {
+    compiler.plugin('compilation', handleCompilation)
+  }
 }
 
 const handleCompilation = (compilation) => {
   compilation.plugin('html-webpack-plugin-before-html-processing', beforeHtml)
 }
 
-SPAWebpackPlugin.prototype.apply = function (compiler) {
-  compiler.plugin('compilation', handleCompilation)
+const beforeHtml = (htmlPluginData, callback) => {
+  const serverCode = require('../build/js/server')
+  htmlPluginData.html = htmlPluginData.html.replace('<!-- server -->', serverCode)
+  callback(null, htmlPluginData)
 }
 
 module.exports = SPAWebpackPlugin
